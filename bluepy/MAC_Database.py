fc5defe1f50ae7bff7abb9b5316a581ef96f5f94
ChiaGolin <chia.golin@gmail.com>
2018-09-22 20:40:34 +0200

    Modifica codice blescan e aggiunta databese e metodo che lo controlla

---
+++ bluepy/MAC_Database.py
@@ -0,0 +1,92 @@
+import sqlite3
+import time
+
+
+conn=sqlite3.connect('MAC_database.db')
+c=conn.cursor()
+
+def create_table():
+    c.execute('CREATE TABLE IF NOT EXISTS Beacon( beacon text, MAC text)')
+
+
+def data_entry():
+    c.execute("INSERT INTO Beacon VALUES('A', 'e1:ab:9e:42:aa:0a')")
+    conn.commit()
+    c.close()
+    conn.close()
+
+def dynamic_data_entry(beacon, MAC):
+    c.execute("INSERT INTO Beacon (beacon, MAC) VALUES (?,?)",
+              (beacon, MAC))
+    conn.commit()
+
+
+def read_beaconID(val):
+    c.execute('SELECT beacon FROM Beacon WHERE beacon IS NOT NULL AND MAC=(?)', [val])
+
+
+    #print(c.fetchall())
+    BeaconID=[]
+
+    for row in c.fetchall():
+
+       BeaconID.append(str(row[0][0]))
+       print('>>>>>>>>>>>BEACON: '+row[0][0])
+
+
+    return BeaconID
+
+
+def read_MAClist(BeaconID):
+
+    list=[]
+
+    c.execute('SELECT MAC FROM Beacon WHERE MAC IS NOT NULL AND beacon=(?)', (BeaconID,))
+
+
+    for row in c.fetchall():
+
+        list.append(row[0])
+      #  print(list)
+
+   # print(list)
+
+    return list
+
+
+
+
+def create_db():
+
+    create_table()
+    ans=input("Do you need to insert a new line: [y/n]")
+    while ans=='y':
+        beacon=input("Insert Beacon: ")
+        MAC=input("insert MAC: ")
+        dynamic_data_entry(beacon, MAC)
+        time.sleep(1)
+        ans = input("Do you need to insert a new line: [y/n]")
+
+
+def search_in_DB(MAC):
+    conn = sqlite3.connect('MAC_database.db')
+    c = conn.cursor()
+    MAC_List=[]
+    BeaconID=read_beaconID(MAC)
+
+
+    if len(BeaconID)>0:
+        print(BeaconID[0][0])
+        MAclist=read_MAClist(BeaconID[0][0])
+
+        for i in range(0, len(MAclist)):
+            MAC_List.append(MAclist[i])
+
+        print(MAC_List[0])
+        c.close()
+        conn.close()
+        return MAC_List
+    else:
+        return "not in range"
+
+
--- bluepy/blescan.py
+++ bluepy/blescan.py
@@ -6,10 +6,9 @@ import os
 import sys
 from bluepy import btle
 import pprint as pp
-import randomize
-
-bluetooth_devices = {}
-bluetooth_device_all = {}
+import hex_conv as hh
+import string
+import MAC_Database as MAC
 
 if os.getenv('C', '1') == '0':
     ANSI_RED = ''
@@ -27,126 +26,15 @@ else:
     ANSI_WHITE = ANSI_CSI + '37m'
     ANSI_OFF = ANSI_CSI + '0m'
 
-def create_dev_tree(bluetooth_devices):
-    conn = 0
-    non_conn = 0
+bluetooth_devices = {}
+bd_list = {}
+bd_list2 = {}
 
-    c_manuf = 0
-    c_m_rand = 0
-    c_m_pub = 0
-    c_m_r_nam = 0
-    c_m_r_unam = 0
-    c_m_p_nam = 0
-    c_m_p_unam = 0
-
-
-    c_non_manuf = 0
-    c_nm_rand = 0
-    c_nm_pub = 0
-    c_nm_r_nam = 0
-    c_nm_r_unam = 0
-    c_nm_p_nam = 0
-    c_nm_p_unam = 0
-
-
-    for key, val in bluetooth_devices.items():
-        if val['conn'] == 'connectable':
-            conn +=1
-
-            if val['manufacturer'] == None:
-                c_non_manuf += 1
-                if val['addr_type'] == 'public':
-                    c_nm_pub += 1
-                    if val['name'] == None:
-                        c_mm_p_unam += 1
-                    else:
-                        c_nm_p_nam += 1
-                elif val['addr_type'] == 'random':
-                    c_m_rand += 1
-                    if val['name'] == None:
-                        c_nm_r_unam += 1
-                    else:
-                        c_nm_r_nam += 1
-            else:
-                c_manuf += 1
-                if val['addr_type'] == 'public':
-                    c_m_pub += 1
-                    if val['name'] == None:
-                        c_m_p_unam += 1
-                    else:
-                        c_m_p_nam += 1
-                elif val['addr_type'] == 'random':
-                    c_m_rand += 1
-                    if val['name'] == None:
-                        c_m_r_unam += 1
-                    else:
-                        c_m_r_nam += 1
-
-
-        else:
-            non_conn +=1
-
-    print ("conn: ", conn)
-    print ("\tmanufacturer: ", c_manuf)
-    print ("\t\trandom: ", c_m_rand)
-    print ("\t\t\tnamed: ", c_m_r_nam)
-    print ("\t\t\tunnamed: ", c_m_r_unam)
-    print ("\t\tpublic: ", c_m_pub)
-    print ("\t\t\tnamed: ", c_m_p_nam)
-    print ("\t\t\tunnamed: ", c_m_p_unam)
-    print ("\tnon manufacturer: ", c_non_manuf)
-    print ("\t\trandom: ", c_nm_rand)
-    print ("\t\t\tnamed: ", c_nm_r_nam)
-    print ("\t\t\tunnamed: ", c_nm_r_unam)
-    print ("\t\tpublic: ", c_nm_pub)
-    print ("\t\t\tnamed: ", c_nm_p_nam)
-    print ("\t\t\tunnamed: ", c_nm_p_unam)
-    print ("non conn", non_conn)
-
-
-def printInfo(bt_devices):
-    n_conn = 0
-    n_non_conn = 0
-    n_non_manufacturer = 0
-    n_manufacturer = 0
-    n_random = 0
-    n_public = 0
-    n_named = 0
-    n_unnamed = 0
-
-    for key, value in bt_devices.items():
-        
-        if value['conn'] == 'connectable':
-            n_conn +=1 
-        else:
-            n_non_conn +=1
-
-        if value['manufacturer'] == None:
-            n_non_manufacturer += 1
-        else:
-            n_manufacturer += 1
-
-        if value['addr_type'] == 'random':
-            n_random +=1
-        else:
-            n_public +=1
-
-        if value['name'] == None:
-            n_unnamed += 1
-        else:
-            n_named += 1
-
-    print("Total devices \"unique\": ", len(bluetooth_devices))
-    print("Total device all", len(bluetooth_device_all))
-    print('conn, non conn:', n_conn, n_non_conn)
-    print('manu, non manu:', n_non_manufacturer, n_manufacturer)
-    print('random, public:', n_random, n_public)
-    print('named, unnamed', n_named, n_unnamed)
 
 def dump_services(dev):
     services = sorted(dev.services, key=lambda s: s.hndStart)
     for s in services:
-        print ("\t%04x: %s" % (s.hndStart, s))
+        print("\t%04x: %s" % (s.hndStart, s))
         if s.hndStart == s.hndEnd:
             continue
         chars = s.getCharacteristics()
@@ -157,14 +45,14 @@ def dump_services(dev):
                 val = c.read()
                 if c.uuid == btle.AssignedNumbers.device_name:
                     string = ANSI_CYAN + '\'' + \
-                        val.decode('utf-8') + '\'' + ANSI_OFF
+                             val.decode('utf-8') + '\'' + ANSI_OFF
                 elif c.uuid == btle.AssignedNumbers.device_information:
                     string = repr(val)
                 else:
                     string = '<s' + binascii.b2a_hex(val).decode('utf-8') + '>'
             else:
                 string = ''
-            print ("\t%04x:    %-59s %-12s %s" % (h, c, props, string))
+            print("\t%04x:    %-59s %-12s %s" % (h, c, props, string))
 
             while True:
                 h += 1
@@ -172,8 +60,8 @@ def dump_services(dev):
                     break
                 try:
                     val = dev.readCharacteristic(h)
-                    print ("\t%04x:     <%s>" %
-                           (h, binascii.b2a_hex(val).decode('utf-8')))
+                    print("\t%04x:     <%s>" %
+                          (h, binascii.b2a_hex(val).decode('utf-8')))
                 except btle.BTLEException:
                     break
 
@@ -187,7 +75,6 @@ class ScanPrint(btle.DefaultDelegate):
     def handleDiscovery(self, dev, isNewDev, isNewData):
         if isNewDev:
             status = "new"
-            devName = None
         elif isNewData:
             if self.opts.new:
                 return
@@ -201,59 +88,41 @@ class ScanPrint(btle.DefaultDelegate):
         if dev.rssi < self.opts.sensitivity:
             return
 
-        
-        print ('    Device (%s): %s (%s), %d dBm %s' %
-               (status,
-                   ANSI_WHITE + dev.addr + ANSI_OFF,
-                   dev.addrType,
-                   dev.rssi,
-                   ('(connectable)' if dev.connectable else '(not connectable)'))
-               )
-        
-
+        print('    Device (%s): %s (%s), %d dBm %s' %
+              (status,
+               ANSI_WHITE + dev.addr + ANSI_OFF,
+               dev.addrType,
+               dev.rssi,
+               ('(connectable)' if dev.connectable else '(not connectable)'))
+              )
         for (sdid, desc, val) in dev.getScanData():
             if sdid in [8, 9]:
-                print ('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')
-                devName = (val if 'Name' in desc else None)
+                print('\t' + desc + ': \'' + ANSI_CYAN + val + ANSI_OFF + '\'')
             else:
-                print ('\t' + desc + ': <' + val + '>')
+                print('\t' + desc + ': <' + val + '>')
                 manufacturer = (val if 'Manufacturer' in desc else None)
- 
+
         if not dev.scanData:
-            print ('\t(no data)')
-                #store devices
+            print('\t(no data)')
 
+        bluetooth_devices[dev.addr] = \
+            {'rssi': dev.rssi,
+             'conn': ('connectable' if dev.connectable else 'not connectable'),
+             'add_type': dev.addrType,
+             'manufacturer': manufacturer}
 
-        siblings = randomize.generate_possible_siblings(dev.addr)
+        bd_list[dev.addr] = \
+            {
+                'rssi': dev.rssi
+                # 'bd_addr': dev.addr
+            }
 
-        is_in = False
-        in_list = []
+        bd_list2[dev.addr] = {
+            'rssi': dev.rssi
+        }
+        # minRSSI_bd
 
-        bluetooth_device_all[dev.addr] = \
-                {'rssi': dev.rssi,
-                 'conn': ('connectable' if dev.connectable else 'not connectable'),
-                 'addr_type': dev.addrType,
-                 'manufacturer': manufacturer, 
-                 'name': devName
-                 }
-
-        for mac in siblings:
-            if mac in bluetooth_devices:
-                is_in = True
-                mac_father = mac
-        
-        if not is_in:
-            bluetooth_devices[dev.addr] = \
-                {'rssi': dev.rssi,
-                 'conn': ('connectable' if dev.connectable else 'not connectable'),
-                 'addr_type': dev.addrType,
-                 'manufacturer': manufacturer, 
-                 'name': devName,
-                 'derivate': []} 
-        else:
-           bluetooth_devices[mac_father]['derivate'].append(dev.addr)
-
-        print
+        print()
 
 
 def main():
@@ -278,28 +147,87 @@ def main():
 
     scanner = btle.Scanner(arg.hci).withDelegate(ScanPrint(arg))
 
-    print (ANSI_RED + "Scanning for devices..." + ANSI_OFF)
+    print(ANSI_RED + "Scanning for devices..." + ANSI_OFF)
     devices = scanner.scan(arg.timeout)
 
     if arg.discover:
-        print (ANSI_RED + "Discovering services..." + ANSI_OFF)
+        print(ANSI_RED + "Discovering services..." + ANSI_OFF)
 
         for d in devices:
             if not d.connectable:
-
                 continue
 
-            print ("    Connecting to", ANSI_WHITE + d.addr + ANSI_OFF + ":")
+            print("    Connecting to", ANSI_WHITE + d.addr + ANSI_OFF + ":")
 
             dev = btle.Peripheral(d)
             dump_services(dev)
             dev.disconnect()
-            print
+            print()
+
+
+def check(val1, val2, n):
+    for i in range(0, n):
+        if val2 == val1 + i or val2 == val1 - i or val1 == val2:
+            result = 1
+            return result
+        else:
+            result = 0
+
+        return result
+
+
+def RSSI_ave(list_RSSI):
+    average = sum(list_RSSI) / len(list_RSSI)
+    return average
+
 
 if __name__ == "__main__":
     main()
-    pp.pprint(bluetooth_devices)
-    printInfo(bluetooth_devices)
+    ble_list = []
+    # pp.pprint(bluetooth_devices)
+    # print('**************************')
+    # pp.pprint(bd_list)
 
-    create_dev_tree(bluetooth_devices)
-#ciaoooooo00000
+    key_list = []
+    rssi_list = []
+    bd_addr = []
+    ################### create list #################
+    for key, val in bd_list.items():
+        print(key, val)
+        # print(val['rssi'])
+        if key != None:
+            rssi_list.append(int(val['rssi']))
+
+    ############# find beacon address (max rssi) ######## if it's the first step
+
+    for key, val in bd_list.items():
+        if int(val['rssi']) == max(rssi_list):
+            bd_addr.append(key)
+
+    ############ select from the database all the address associated to the same beacon ############
+    print('>>>>>>>>>')
+    print(bd_addr)
+    print('><>>>>>>>>><')
+    for i in range(0, len(bd_addr)):
+        print("Searching " + bd_addr[i])
+        MAC_List = MAC.search_in_DB(
+            bd_addr[i])  # I'll send the list to all the rasberrypi, or just one MAC and I'll find the
+        # other with the same procedure of above
+        # print('ter')
+        # print(MAC_List)
+        RSSI_list = []
+        # print(MAC_List)
+        if MAC_List != 'not in range':
+            for key, val in bd_list.items():
+                if key in MAC_List:
+                    # print(val['rssi'])
+                    if val['rssi'] != None:
+                        RSSI_list.append(int(val['rssi']))
+
+            print(RSSI_list)
+            RSSI_average = RSSI_ave(RSSI_list)
+            print(RSSI_average)
+
+        else:
+            print("############### BEACON NOT FOUND ##################")
+
